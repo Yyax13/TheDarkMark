46778e9c97f4c77c78ba1a4390c78bac39e88973
Yyax13 <Yyax13@proton.me>
2025-08-04 15:08:56 +0200

    REFACTOR - Removed useless prefixes ("[SYSTEM]", "[WARN]") from logging

--- src/misc/logging.go
+++ src/misc/logging.go
@@ -3,26 +3,24 @@ package misc
 import "fmt"
 
 func PanicWarn(logContent string, newLine bool) {
-	var warnText, _ = Colors("[WARN]", "red")
 	if newLine {
-		warnText = "\n" + warnText
+		fmt.Println()
 
 	}
 
 	logContentColored, _ := Colors(logContent, "yellow")
-	fmt.Printf("%s %v", warnText, logContentColored)
+	fmt.Printf("%v", logContentColored)
 
 }
 
 func SysLog(logContent string, newLine bool) {
-	var sysText, _ = Colors("[SYSTEM]", "black")
 	if newLine {
-		sysText = "\n" + sysText
+		fmt.Println()
 
 	}
 	
 	logContentColored, _ := Colors(logContent, "white")
 
-	fmt.Printf("%s %v", sysText, logContentColored)
+	fmt.Printf("%v", logContentColored)
 
 }
