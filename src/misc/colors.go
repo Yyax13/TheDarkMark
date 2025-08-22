d53168cba10fe208f05e9d9e6ce0dd9cd5452f90
Yyax13 <Yyax13@proton.me>
2025-08-22 20:49:28 +0200

    FEAT - Implemented XOR encryption function

--- src/misc/colors.go
+++ src/misc/colors.go
@@ -5,71 +5,71 @@ import (
 	"fmt"
 )
 
-var avaliableColors map[string]string = map[string]string{
-    "red":          "\033[38;2;255;0;0m",
-    "green":        "\033[38;2;0;255;0m",
-    "blue":         "\033[38;2;0;0;255m",
-    "yellow":       "\033[38;2;255;255;0m",
-    "magenta":      "\033[38;2;255;0;255m",
-    "cyan":         "\033[38;2;0;255;255m",
-    "white":        "\033[38;2;255;255;255m",
-    "black":        "\033[38;2;0;0;0m",
+var AvaliableColors map[string]string = map[string]string{
+	"red":          "\x1b[38;2;215;0;0m",
+	"green":        "\x1b[38;2;0;215;0m",
+	"blue":         "\x1b[38;2;0;0;215m",
+	"yellow":       "\x1b[38;2;215;215;0m",
+	"magenta":      "\x1b[38;2;215;0;215m",
+	"cyan":         "\x1b[38;2;0;215;215m",
+	"white":        "\x1b[38;2;215;215;215m",
+	"black":        "\x1b[38;2;0;0;0m",
 
-    "red_bold":     "\033[1;38;2;255;0;0m",
-    "green_bold":   "\033[1;38;2;0;255;0m",
-    "blue_bold":    "\033[1;38;2;0;0;255m",
-    "yellow_bold":  "\033[1;38;2;255;255;0m",
-    "magenta_bold": "\033[1;38;2;255;0;255m",
-    "cyan_bold":    "\033[1;38;2;0;255;255m",
-    "white_bold":   "\033[1;38;2;255;255;255m",
-    "black_bold":   "\033[1;38;2;0;0;0m",
+	"red_bold":     "\x1b[1;38;2;215;0;0m",
+	"green_bold":   "\x1b[1;38;2;0;215;0m",
+	"blue_bold":    "\x1b[1;38;2;0;0;215m",
+	"yellow_bold":  "\x1b[1;38;2;215;215;0m",
+	"magenta_bold": "\x1b[1;38;2;215;0;215m",
+	"cyan_bold":    "\x1b[1;38;2;0;215;215m",
+	"white_bold":   "\x1b[1;38;2;215;215;215m",
+	"black_bold":   "\x1b[1;38;2;0;0;0m",
 
-    "light_red":    "\033[38;2;255;70;70m",
-    "light_green":  "\033[38;2;70;255;70m",
-    "light_blue":   "\033[38;2;70;70;255m",
-    "light_yellow": "\033[38;2;255;255;70m",
-    "light_magenta":"\033[38;2;255;70;255m",
-    "light_cyan":   "\033[38;2;70;255;255m",
-    "light_white":  "\033[38;2;255;255;255m",
-    "light_black":  "\033[38;2;70;70;70m",
+	"light_red":    "\x1b[38;2;240;0;0m",
+	"light_green":  "\x1b[38;2;0;240;0m",
+	"light_blue":   "\x1b[38;2;0;0;240m",
+	"light_yellow": "\x1b[38;2;240;240;0m",
+	"light_magenta":"\x1b[38;2;240;0;240m",
+	"light_cyan":   "\x1b[38;2;0;240;240m",
+	"light_white":  "\x1b[38;2;240;240;240m",
+	"light_black":  "\x1b[38;2;60;60;60m",
 
-    "light_red_bold":    "\033[1;38;2;255;70;70m",
-    "light_green_bold":  "\033[1;38;2;70;255;70m",
-    "light_blue_bold":   "\033[1;38;2;70;70;255m",
-    "light_yellow_bold": "\033[1;38;2;255;255;70m",
-    "light_magenta_bold":"\033[1;38;2;255;70;255m",
-    "light_cyan_bold":   "\033[1;38;2;70;255;255m",
-    "light_white_bold":  "\033[1;38;2;255;255;255m",
-    "light_black_bold":  "\033[1;38;2;70;70;70m",
+	"light_red_bold":    "\x1b[1;38;2;240;0;0m",
+	"light_green_bold":  "\x1b[1;38;2;0;240;0m",
+	"light_blue_bold":   "\x1b[1;38;2;0;0;240m",
+	"light_yellow_bold": "\x1b[1;38;2;240;240;0m",
+	"light_magenta_bold":"\x1b[1;38;2;240;0;240m",
+	"light_cyan_bold":   "\x1b[1;38;2;0;240;240m",
+	"light_white_bold":  "\x1b[1;38;2;240;240;240m",
+	"light_black_bold":  "\x1b[1;38;2;60;60;60m",
 
-    "dark_red":     "\033[38;2;185;0;0m",
-    "dark_green":   "\033[38;2;0;185;0m",
-    "dark_blue":    "\033[38;2;0;0;185m",
-    "dark_yellow":  "\033[38;2;185;185;0m",
-    "dark_magenta": "\033[38;2;185;0;185m",
-    "dark_cyan":    "\033[38;2;0;185;185m",
-    "dark_white":   "\033[38;2;185;185;185m",
-    "dark_black":   "\033[38;2;0;0;0m",
+	"dark_red":     "\x1b[38;2;110;0;0m",
+	"dark_green":   "\x1b[38;2;0;110;0m",
+	"dark_blue":    "\x1b[38;2;0;0;110m",
+	"dark_yellow":  "\x1b[38;2;110;110;0m",
+	"dark_magenta": "\x1b[38;2;110;0;110m",
+	"dark_cyan":    "\x1b[38;2;0;110;110m",
+	"dark_white":   "\x1b[38;2;110;110;110m",
+	"dark_black":   "\x1b[38;2;0;0;0m",
 
-    "dark_red_bold":     "\033[1;38;2;185;0;0m",
-    "dark_green_bold":   "\033[1;38;2;0;185;0m",
-    "dark_blue_bold":    "\033[1;38;2;0;0;185m",
-    "dark_yellow_bold":  "\033[1;38;2;185;185;0m",
-    "dark_magenta_bold": "\033[1;38;2;185;0;185m",
-    "dark_cyan_bold":    "\033[1;38;2;0;185;185m",
-    "dark_white_bold":   "\033[1;38;2;185;185;185m",
-    "dark_black_bold":   "\033[1;38;2;0;0;0m",
+	"dark_red_bold":     "\x1b[1;38;2;110;0;0m",
+	"dark_green_bold":   "\x1b[1;38;2;0;110;0m",
+	"dark_blue_bold":    "\x1b[1;38;2;0;0;110m",
+	"dark_yellow_bold":  "\x1b[1;38;2;110;110;0m",
+	"dark_magenta_bold": "\x1b[1;38;2;110;0;110m",
+	"dark_cyan_bold":    "\x1b[1;38;2;0;110;110m",
+	"dark_white_bold":   "\x1b[1;38;2;110;110;110m",
+	"dark_black_bold":   "\x1b[1;38;2;0;0;0m",
 
-    "reset":        "\033[0m",
+	"reset":        "\x1b[0m",
 }
 
 func Colors(text, color string) (string, error) {
-	if _, exists := avaliableColors[color]; !exists {
+	if _, exists := AvaliableColors[color]; !exists {
 		return "", errors.New("color not avaliable")
 
 	}
 
-	return fmt.Sprintf("%s%v%s", avaliableColors[color], text, avaliableColors["reset"]), nil
+	return fmt.Sprintf("%s%v%s", AvaliableColors[color], text, AvaliableColors["reset"]), nil
 
 }
 
---
+++ src/misc/xor.go
@@ -0,0 +1,14 @@
+package misc
+
+func Xor(plain, key []byte) ([]byte) {
+	keyCopy := key
+	xorResult := make([]byte, len(plain))
+	for i := range len(plain) {
+		xorResult[i] = xorResult[i] ^ keyCopy[(i+1)%len(keyCopy)]
+		keyCopy[(i+2)%len(keyCopy)] = keyCopy[i%len(keyCopy)]
+
+	}
+
+	return xorResult
+	
+}
