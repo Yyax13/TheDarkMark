d09bf8fdf67290358cb82d4187c21031550b2876
Yyax13 <Yyax13@proton.me>
2025-09-15 20:17:03 +0200

    FIX - Fixed keyCopy to really copy the key; Fixed incorrect use of range in the for loop

--- src/misc/xor.go
+++ src/misc/xor.go
@@ -1,9 +1,9 @@
 package misc
 
 func Xor(plain, key []byte) ([]byte) { // better than RSA man, this is very nice 'cause just sniffing into traffic will not help the blue team :D
-	keyCopy := key
+	keyCopy := append([]byte(nil), key...)
 	xorResult := make([]byte, len(plain))
-	for i := range len(plain) {
+	for i := range plain {
 		xorResult[i] = plain[i] ^ keyCopy[(i+1)%len(keyCopy)]
 		keyCopy[(i+2)%len(keyCopy)] = keyCopy[i%len(keyCopy)]
 
