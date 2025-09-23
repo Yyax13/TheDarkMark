b173c982a8816b79e10c2096782fb97941272027
Yyax13 <Yyax13@proton.me>
2025-09-23 22:03:34 +0200

    FEAT - Added x86 Linux basic reverse shell implementation with command handling and retry mechanisms

---
+++ src/spells/x86linux/basic/reverse_shell/macros.h
@@ -0,0 +1,120 @@
+// options (malfoy)
+
+#ifndef PROTOCOL_ENCODER_NAME
+#define PROTOCOL_ENCODER_NAME "placeholder"
+#endif
+
+#ifndef PAYLOAD_ENCODER_NAME
+#define PAYLOAD_ENCODER_NAME "placeholder"
+#endif
+
+#ifndef PROTOCOL_NAME
+#define PROTOCOL_NAME "placeholder"
+#endif
+
+#ifndef LHOST
+#define LHOST "placeholder"
+#endif
+
+#ifndef LPORT
+#define LPORT "placeholder"
+#endif
+
+#ifndef TIMEOUT
+#define TIMEOUT "placeholder"
+#endif
+
+#ifndef SLEEP
+#define SLEEP "placeholder"
+#endif
+
+#ifndef BEACON_TIME
+#define BEACON_TIME "placeholder"
+#endif
+
+#ifndef JITTER
+#define JITTER "placeholder"
+#endif
+
+#ifndef RETRY_METHOD
+#define RETRY_METHOD "placeholder"
+#endif
+
+#ifndef RETRY_DELAY
+#define RETRY_DELAY "placeholder"
+#endif
+
+#ifndef RETRY_ATTEMPTS_CAP
+#define RETRY_ATTEMPTS_CAP "placeholder"
+#endif
+
+// params (fidelius/ritual)
+#ifndef PAYLOAD_ENCODER_KEY
+#define PAYLOAD_ENCODER_KEY "placeholder"
+#endif
+
+#ifndef PROTOCOL_ENCODER_KEY
+#define PROTOCOL_ENCODER_KEY "placeholder"
+#endif
+
+// hardcoded strings
+#ifndef BEACON_COMMANDS_EXEC
+#define BEACON_COMMANDS_EXEC "placeholder"
+#endif
+
+#ifndef BEACON_COMMANDS_GET_BOT_DATA
+#define BEACON_COMMANDS_GET_BOT_DATA "placeholder"
+#endif
+
+#ifndef ENV_HISTFILE
+#define ENV_HISTFILE "placeholder"
+#endif
+
+#ifndef PLACEHOLDER_VAL
+#define PLACEHOLDER_VAL "placeholder"
+#endif
+
+#ifndef RETRY_METHOD_NONE
+#define RETRY_METHOD_NONE "placeholder"
+#endif
+
+#ifndef RETRY_METHOD_FIXED
+#define RETRY_METHOD_FIXED "placeholder"
+#endif
+
+#ifndef RETRY_METHOD_LINEAR
+#define RETRY_METHOD_LINEAR "placeholder"
+#endif
+
+#ifndef RETRY_METHOD_EXPONENTIAL
+#define RETRY_METHOD_EXPONENTIAL "placeholder"
+#endif
+
+#ifndef RETRY_METHOD_EXPONENTIAL_JITTER
+#define RETRY_METHOD_EXPONENTIAL_JITTER "placeholder"
+#endif
+
+#ifndef REQUEST_FROM_C2_FAILED
+#define REQUEST_FROM_C2_FAILED "placeholder"
+#endif
+
+#ifndef BIN_SH_PATH
+#define BIN_SH_PATH "placeholder"
+#endif
+
+#ifndef PROC_CPUINFO_PATH
+#define PROC_CPUINFO_PATH "placeholder"
+#endif
+
+#ifndef HOSTNAME_PATH
+#define HOSTNAME_PATH "placeholder"
+#endif
+
+#ifndef OS_RELEASE_PATH
+#define OS_RELEASE_PATH "placeholder"
+#endif
+
+#ifndef UPTIME_PATH
+#define UPTIME_PATH "placeholder"
+#endif
+
