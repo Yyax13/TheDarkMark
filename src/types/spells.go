5d294a53da91268b3199a408761ea1289f5d37da
Yyax13 <Yyax13@proton.me>
2025-09-23 22:03:00 +0200

    REFACTOR - Improved structure of Spell type by adding Macros and Methods fields

--- src/types/spells.go
+++ src/types/spells.go
@@ -1,9 +1,26 @@
 package types
 
+type SpellMacro struct{
+	Macro 					string
+	Value					any
+
+}
+
+type SpellMethod struct{
+	Name					string
+	Description				string
+	UsageExample			string
+	OperatorSideCommand		string
+	ImplantSideCommand		string
+}
+
 type Spell struct{
-	Name			string
-	Description		string
-	CFilePath		string
+	Name					string
+	Description				string
+	PayloadAsoluteDirPath	string
+	Methods					map[string]*SpellMethod
+	InsertCommand			func(ImplantSideCommand string, originalData []byte) ([]byte, error)
+	Macros					map[string]*SpellMacro
 
 }
 
